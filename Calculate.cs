f574303c37b079771bfb1a676d946e08fc12849b
PdNair <104288272+PdNair@users.noreply.github.com>
2022-04-24 09:02:25 +0530

    Add files via upload

---
+++ Calculate.cs
@@ -0,0 +1,126 @@
+﻿using MarsRoversProblem;
+using System;
+
+namespace MarsRoverProblem
+{
+    public interface ICalculate
+    {
+        void Navigate(string command);
+    }
+
+    public class Calculate : ICalculate
+    {
+        public int xLimit { get; set; }
+        public int yLimit { get; set; }
+        public int X { get; set; }
+        public int Y { get; set; }
+        public Directions Direction { get; set; }
+
+        public Calculate()
+        {
+            X = Y = 1;
+            Direction = Directions.North;
+        }
+
+        private void TurnLeft()
+        {
+            switch (Direction)
+            {
+                case Directions.North:
+                    Direction = Directions.West;
+                    break;
+                case Directions.South:
+                    Direction = Directions.East;
+                    break;
+                case Directions.East:
+                    Direction = Directions.North;
+                    break;
+                case Directions.West:
+                    Direction = Directions.South;
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private void TurnRight()
+        {
+            switch (Direction)
+            {
+                case Directions.North:
+                    Direction = Directions.East;
+                    break;
+                case Directions.South:
+                    Direction = Directions.West;
+                    break;
+                case Directions.East:
+                    Direction = Directions.South;
+                    break;
+                case Directions.West:
+                    Direction = Directions.North;
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private void MoveForward()
+        {
+            switch (Direction)
+            {
+                case Directions.North:
+                    Y++;
+                    CheckLimit();
+                    break;
+                case Directions.South:
+                    Y--;
+                    CheckLimit();
+                    break;
+                case Directions.East:
+                    X++;
+                    CheckLimit();
+                    break;
+                case Directions.West:
+                    X--;
+                    CheckLimit();
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private void CheckLimit()
+        {
+            if (X >= xLimit)
+                X = xLimit;
+            if (Y >= yLimit)
+                Y = yLimit;
+            if (X <= 1)
+                X = 1;
+            if (Y <= 1)
+                Y = 1;
+        }
+
+        public void Navigate(string commandSet)
+        {
+            foreach (char command in commandSet)
+            {
+                switch (command)
+                {
+                    case 'F':
+                        MoveForward();
+                        break;
+                    case 'L':
+                        TurnLeft();
+                        break;
+                    case 'R':
+                        TurnRight();
+                        break;
+                    default:
+                        Console.WriteLine($"Invalid Character {command}");
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using MarsRoverProblem;
+using System;
+
+namespace MarsRoversProblem
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            UserInput();
+        }
+
+        private static void UserInput()
+        {
+            Console.WriteLine("Enter the grid size in specific format (eg- 3*5 4*5 5*5)");
+            string gridSize = Console.ReadLine().Replace(" ", "");
+
+            Console.WriteLine("Enter the navigation command (eg - L R F)");
+            string commandSet = Console.ReadLine().ToUpper().Replace(" ", "");
+
+            Start(gridSize, commandSet);
+        }
+
+        private static void Start(string gridSize, string commandSet)
+        {
+            try
+            {
+                Calculate calc = new Calculate
+                {
+                    xLimit = Convert.ToInt32(gridSize.Substring(0, gridSize.IndexOf("*"))),
+                    yLimit = Convert.ToInt32(gridSize.Substring(gridSize.LastIndexOf('*') + 1))
+                };
+                calc.Navigate(commandSet);
+                Console.WriteLine($"Final Position - {calc.X}, {calc.Y}, {calc.Direction} \n");
+
+                Console.WriteLine("Press 1 to continue. Press 'Enter' to exit");
+                ConsoleKeyInfo keyInfo = Console.ReadKey();
+                if (keyInfo.Key != ConsoleKey.Enter)
+                {
+                    Console.WriteLine("\n");
+                    UserInput();
+                }   
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Halted with an Error - ", ex.Message);
+            }
+        }
+    }
+}
